3f6e5c2d21be73b94ae01bf674c340b61e299902
max <mail.max.khomenko@gmail.com>
2021-06-22 13:42:53 +0300

    Passed proper parameters to ctor

--- main.cpp
+++ main.cpp
@@ -7,12 +7,16 @@
 #include "sound_encoder.h"
 #include "sound_decoder.h"
 
+#include "audio_pipeline.h"
+
 int main(int argc, char *argv[]) {
     using namespace std::chrono_literals;
     QCoreApplication app(argc, argv);
 
     QThread audio;
-    SoundEncoder encoder(QAudioDeviceInfo::defaultInputDevice());
+
+    const QAudioDeviceInfo &deviceInfo = QAudioDeviceInfo::defaultInputDevice();
+    SoundEncoder encoder(deviceInfo, AudioPipeline::setupAudioFormat(deviceInfo));
     encoder.moveToThread(&audio);
 
     SoundDecoder decoder(QAudioDeviceInfo::defaultOutputDevice(), encoder.format());
