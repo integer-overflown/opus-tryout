f71aab2410f464270aff61071466666de39588f1
max <mail.max.khomenko@gmail.com>
2021-06-23 15:31:01 +0300

    Finished encoder device

--- encoder_device.cpp
+++ encoder_device.cpp
@@ -1,7 +1,14 @@
 #include "encoder_device.h"
 #include <QDebug>
+#include <limits>
+
+EncoderDevice::EncoderDevice(EncoderDevice::Encoder encoder) : encoder_(std::move(encoder)) {}
 
 qint64 EncoderDevice::writeData(const char *data, qint64 len) {
-    if (auto pipe = getPipe()) pipe->write(data, len);
+    Q_ASSERT(len <= std::numeric_limits<int>::max());
+    if (auto pipe = getPipe(); pipe && encoder_) {
+        auto enc = encoder_(QByteArray::fromRawData(data, static_cast<int>(len)));
+        pipe->write(enc);
+    }
     return len;
 }
--- encoder_device.h
+++ encoder_device.h
@@ -3,8 +3,14 @@
 #include "pipe_device.h"
 
 class EncoderDevice : public PipeDevice {
+public:
+    using Encoder = std::function<QByteArray(QByteArray)>;
+public:
+    explicit EncoderDevice(Encoder);
 protected:
     qint64 writeData(const char *data, qint64 len) override;
+private:
+    Encoder encoder_;
 };
 
 
